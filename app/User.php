e9d73d17755b97c971755d627de8255acebd4c11
chaosaround <v.kryzhnii@gmail.com>
2020-02-02 23:37:33 +0200

    Boolean fields casts for User model

--- app/User.php
+++ app/User.php
@@ -35,6 +35,8 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_approved' => 'boolean',
+        'is_admin' => 'boolean'
     ];
 
 	/**
